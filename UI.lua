da10d03ead33d923915574e02d96d5f455880230
Jay1086086 <2735312317@qq.com>
2025-08-30 18:47:27 +0800

    11

--- UI.lua
+++ UI.lua
@@ -93,23 +93,10 @@ end)
         StrokeThickness = 3,  -- 增加边框厚度
     })
     
-    local MainTab = Window:Tab({
-        Title = "主页",
-        Icon = "zap",
-        Locked = false,
-    })
-    
-        local CnmicTab = Window:Tab({
-        Title = "通用",
-        Icon = "zap",
-        Locked = false,
-    })
-    
-        local JaobeneTab = Window:Tab({
-        Title = "其他脚本中心",
-        Icon = "zap",
-        Locked = false,
-    })
+local Tabs = {
+    Main = Window:Tab({ Title = "主页", Icon = "rbxassetid://6026568198" }),
+    CnmicTab = Window:Tab({ Title = "通用", Icon = "rbxassetid://94462465090724" }),
+    JaobeneTab = Window:Tab({ Title = "脚本中心", Icon = "swords" })
     
     MainTab:Paragraph({
         Title = "欢迎使用JAYhub",
